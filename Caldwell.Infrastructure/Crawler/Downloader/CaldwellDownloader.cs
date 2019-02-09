433e777bdff087e47caae3af86d61a0fdd7a9227
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-02-09 23:31:56 +0300

    HtmlDocument returned downloader

--- Caldwell.Infrastructure/Crawler/Downloader/CaldwellDownloader.cs
+++ Caldwell.Infrastructure/Crawler/Downloader/CaldwellDownloader.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using System.Text;
 
@@ -7,24 +9,73 @@ namespace Caldwell.Infrastructure.Crawler.Downloader
 {
     public class CaldwellDownloader : ICaldwellDownloader
     {
-        public void Download(string crawlUrl, CaldwellDownloaderType downloaderType, string fileName = null)
+        private readonly CaldwellDownloaderType _downloaderType;
+
+        public CaldwellDownloader(CaldwellDownloaderType downloaderType)
+        {
+            _downloaderType = downloaderType;
+        }
+
+        public HtmlDocument Download(string crawlUrl, string fileName = null)
         {
             // if exist dont download again
+            if(!string.IsNullOrEmpty(fileName))
+            {
+                GetExistingFile(fileName);
+            }
 
+            switch (_downloaderType)
+            {
+                case CaldwellDownloaderType.FromFile:
+                    DownloadToFile(crawlUrl, fileName);                    
+                    break;
+                case CaldwellDownloaderType.FromMemory:
+                    DownloadToMemory(crawlUrl, fileName);
+                    break;
+                case CaldwellDownloaderType.FromWeb:
+                    return DownloadToWeb(crawlUrl, fileName);
+                    break;
+                default:
+                    break;
+            }
+
+         
+        }
+
+        private HtmlDocument DownloadToWeb(string crawlUrl, string fileName)
+        {
+            HtmlWeb web = new HtmlWeb();
+            return web.Load(crawlUrl);
+        }
+
+        private void DownloadToMemory(string crawlUrl, string fileName)
+        {
             using (WebClient client = new WebClient()) // WebClient class inherits IDisposable
             {
-                client.DownloadFile(crawlUrl, @"C:\mozk_delete\"+ fileName + ".html");
-
                 // Or you can get the file content without saving it
                 string htmlCode = client.DownloadString(crawlUrl);
             }
-        }       
+        }
+
+        private void DownloadToFile(string crawlUrl, string fileName)
+        {
+            using (WebClient client = new WebClient()) // WebClient class inherits IDisposable
+            {
+                client.DownloadFile(crawlUrl, @"C:\mozk_delete\" + fileName + ".html");                
+            }
+        }
+
+        private void GetExistingFile(string fileName)
+        {            
+            string htmlString = File.ReadAllText(fileName);
+            HtmlDocument htmlDocument = 
+        }
     }
 
     public enum CaldwellDownloaderType
     {
-        FromFile,
-        FromString,
-        FromWeb
+        FromFile, // download to local file
+        FromMemory, // without downloading to local
+        FromWeb // read direct from web
     }
 }
