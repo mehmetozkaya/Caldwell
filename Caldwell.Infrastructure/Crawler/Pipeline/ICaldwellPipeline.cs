935cb65a01e2becb5c75629a5761af1fef78e635
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-02-12 09:05:15 +0300

    ICaldwellPipeline

---
+++ Caldwell.Infrastructure/Crawler/Pipeline/ICaldwellPipeline.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Caldwell.Infrastructure.Crawler.Pipeline
+{
+    public interface ICaldwellPipeline
+    {
+    }
+}
