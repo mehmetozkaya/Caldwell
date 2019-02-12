bb5c2f86b52e3c26002d72410f59fa972647909b
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-02-12 09:10:12 +0300

    CaldwellRequest

--- Caldwell.Infrastructure/Crawler/Request/CaldwellRequest.cs
+++ Caldwell.Infrastructure/Crawler/Request/CaldwellRequest.cs
@@ -5,6 +5,7 @@ using System.Text;
 namespace Caldwell.Infrastructure.Crawler.Request
 {
     public class CaldwellRequest : ICaldwellRequest
-    {
+    {        
+        public string TimeOut { get; set; }
     }
 }
