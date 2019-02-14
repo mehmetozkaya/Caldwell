7dd06a24060375b39737ddfcea0bc9bf2d4543f5
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-02-14 07:41:01 +0300

    GetNewLinks

--- Caldwell.Infrastructure/Crawler/Processor/CaldwellProcessor.cs
+++ Caldwell.Infrastructure/Crawler/Processor/CaldwellProcessor.cs
@@ -136,5 +136,20 @@ namespace Caldwell.Infrastructure.Crawler.Processor
             return tasks.SelectMany(l => l);
         }
 
+        // third solution
+        public ISet<string> GetNewLinks(string content)
+        {
+            Regex regexLink = new Regex("(?<=<a\\s*?href=(?:'|\"))[^'\"]*?(?=(?:'|\"))");
+
+            ISet<string> newLinks = new HashSet<string>();
+            foreach (var match in regexLink.Matches(content))
+            {
+                if (!newLinks.Contains(match.ToString()))
+                    newLinks.Add(match.ToString());
+            }
+
+            return newLinks;
+        }
+
     }
 }
